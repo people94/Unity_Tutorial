bf4593c3dd54a8201ad2bc0114783c24c1643518
Seung Yoon Baek <bsyu0804@naver.com>
2020-05-24 21:13:08 +0900

    20. 네비게이션

---
+++ UnityTutorial/Assets/Scripts/NavigationTest.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavigationTest : MonoBehaviour
+{
+
+    Rigidbody myRigid;
+    [SerializeField] private float moveSpeed;
+
+    NavMeshAgent agent;
+
+    //[SerializeField] private Transform tf_Destination;
+    private Vector3 originPos;
+
+    [SerializeField] private Transform[] tf_Destination;
+    private Vector3[] wayPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigid = GetComponent<Rigidbody>();
+        agent = GetComponent<NavMeshAgent>();
+        originPos = transform.position;
+
+        wayPoints = new Vector3[tf_Destination.Length + 1];
+        for (int i = 0; i < tf_Destination.Length; i++)
+        {
+            wayPoints[i] = tf_Destination[i].position;
+        }
+        wayPoints[wayPoints.Length - 1] = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Patrol();
+        //if(Input.GetKeyDown(KeyCode.W))
+        //{
+        //    //myRigid.MovePosition(this.transform.position + transform.forward * moveSpeed * Time.deltaTime);
+        //    agent.SetDestination(tf_Destination.position);
+        //}
+    }
+
+    private void Patrol()
+    {
+        // if (Vector3.Distance(transform.position, tf_Destination.position) < 0.1f)
+        //     agent.SetDestination(originPos);
+        // else if(Vector3.Distance(transform.position, originPos) < 0.1f)
+        //     agent.SetDestination(tf_Destination.position);
+        for (int i = 0; i < wayPoints.Length; i++)  //length = 4, 배열의 인덱스 0 ~ 3
+        {
+            if(Vector3.Distance(transform.position, wayPoints[i]) <= 0.1f)
+            {
+                if(i != wayPoints.Length - 1)
+                    agent.SetDestination(wayPoints[i + 1]);
+                else
+                    agent.SetDestination(wayPoints[0]);
+            }
+        }
+    }
+}
